6c897a2fe910eadf1c4040f0da7eb12e2c5d657e
Joseph Johansen <joe@stotles.com>
2021-09-02 19:10:48 +0100

    Allow default values to be specified upon creation

--- spec/typed_struct_spec.rb
+++ spec/typed_struct_spec.rb
@@ -29,4 +29,50 @@ RSpec.describe TypedStruct do
       expect(y.__class__).to be_an_instance_of Class
     end
   end
+
+  context "when passing options" do
+    it "allows for default values" do
+      x = TypedStruct.new(
+        TypedStruct::Options.new(default: 5),
+        int: Integer,
+        str: String,
+      )
+      y = x.new str: "abc"
+      expect(y.str).to eq "abc"
+      expect(y.int).to eq 5
+    end
+
+    it "allows anything responding to [] to be passed as options" do
+      x = TypedStruct.new(
+        { default: 3 },
+        xyz: /foobar/,
+        abc: :abc,
+      )
+      y = x.new abc: :abc, xyz: "foobarbaz"
+      expect(y.abc).to eq :abc
+      expect(y.xyz).to eq "foobarbaz"
+    end
+
+    it "breaks if a missing type and the type of the default don't match" do
+      x = TypedStruct.new(
+        TypedStruct::Options.new(default: 1),
+        int: Integer,
+        str: String,
+      )
+
+      expect { x.new(int: 4) }.to raise_error "Unexpected type Integer for :str (expected String)"
+    end
+  end
+
+  context "when no options passed" do
+    it "defaults to nil for missing properties" do
+      x = TypedStruct.new abc: Rbs("String?")
+      expect(x.new.abc).to be_nil
+    end
+
+    it "errors if the missing property cannot be nil" do
+      x = TypedStruct.new abc: Rbs("String")
+      expect { x.new }.to raise_error "Unexpected type NilClass for :abc (expected String)"
+    end
+  end
 end
